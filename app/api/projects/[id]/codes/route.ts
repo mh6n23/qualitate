86ce24fcd682bff5b5eb86e49c4815039160871a
mh6n23 <mh6n23@soton.ac.uk>
2026-04-02 17:21:11 +0100

    Created API for codes

---
+++ app/api/projects/[id]/codes/route.ts
@@ -0,0 +1,61 @@
+import {NextResponse} from "next/server";
+import {prisma} from '@/lib/prisma';
+
+// Take in the projectID
+export async function GET(request: Request, context: {params: Promise<{id: string}>}) {
+    const {id} = await context.params;
+    const projectID = parseInt(id);
+
+    const codes = await prisma.code.findMany({
+        where: {projectID},
+        orderBy: {name: "asc"},
+        include: {
+            _count: {
+                select: {annotations: true}
+            }
+        }
+    });
+
+    return NextResponse.json(codes);
+}
+
+export async function POST(request: Request, context: {params: Promise<{id: string}>}) {
+    const {id} = await context.params;
+    const projectID = parseInt(id);
+    const body = await request.json();
+
+    if (!body.name) {
+        return NextResponse.json(
+            {error: "Code name missing"},
+            {status: 400}
+        );
+    }
+    else if (!body.colour) {
+        return NextResponse.json(
+            {error: "Code colour missing"},
+            {status: 400}
+        );
+    }
+
+    const project = await prisma.project.findUnique({
+        where: {id: projectID}
+    });
+
+    if (!project) {
+        return NextResponse.json(
+            {error: "Couldn't find project to add code to"},
+            {status: 404}
+        );
+    }
+
+    const code = await prisma.code.create({
+        data: {
+            name: body.name,
+            description: body.description || null,
+            colour: body.colour,
+            projectID
+        }
+    });
+
+    return NextResponse.json(code, {status: 201})
+}
